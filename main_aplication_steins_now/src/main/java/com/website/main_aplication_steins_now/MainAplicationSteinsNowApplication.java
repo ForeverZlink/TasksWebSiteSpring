2a48b96b70e303cb55ad2d533a0595c3d3b3cc4b
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-21 20:29:26 -0300

    feat:Criado projeto base.
    Descrição: Utilizei o spring initializr para começar com o projeto. Já foi adicionado algumas depedências que serão necessárias para a criação do app. Dentre elas: Sessões, testes, Mysql driver, Responsive, Rest entre outras. O projeto já contém o modelo base.

---
+++ main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/MainAplicationSteinsNowApplication.java
@@ -0,0 +1,13 @@
+package com.website.main_aplication_steins_now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAplicationSteinsNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainAplicationSteinsNowApplication.class, args);
+	}
+
+}
---
+++ main_aplication_steins_now/src/test/java/com/website/main_aplication_steins_now/MainAplicationSteinsNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.website.main_aplication_steins_now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAplicationSteinsNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
