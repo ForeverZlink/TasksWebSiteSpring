4c48f51a961fd9e63be6de42d7fbc2038e31f3d2
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-26 21:59:53 -0300

    feat(Banco de dados): Criado repositório de acesso para o UserModel
    Foi adicionado uma interface para lidar com operações CRUD  do
    UserModel.
    Essa interface é um repository, esse que será possivel
    manusear o banco de dados a partir desta abstração. Com essa abstração,
    por exemplo, será possível criar registros, atualizar e apagar em um
    controller, pois o próprio spring, já faz a injeção de depedência quando
    criamos uma variável do tipo desse repository que acabou de ser feito.

---
+++ main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/repository/UserModelRepository.java
@@ -0,0 +1,9 @@
+package com.website.main_aplication_steins_now.repository;
+
+
+import com.website.main_aplication_steins_now.models.UserModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserModelRepository extends CrudRepository<UserModel, Integer> {
+
+}
