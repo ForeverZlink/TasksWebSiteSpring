ac4c3f34733307e6d107527e4c8e6d038e56154f
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-26 21:53:00 -0300

    feat(Identificação de usuários.): Adicionado alguns campos ao model UserModel
    Adicionei alguns campos para o UserModel, dentre eles:
    Nome, email e
    afins.

--- main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/models/UserModel.java
+++ main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/models/UserModel.java
@@ -5,9 +5,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.lang.reflect.Type;
+import java.util.Date;
+
 @Entity
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer Id;
+
+    public String Username;
+    public String Email;
+    public Date Date_of_Birth;
+
 }
