e8bd24007b4212d30f7d68dc494e7896d893da47
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-05-29 22:54:18 -0300

    feat(Controllers): Criado controller para lidar com entidades do tipo User.
    Esse controller tem o objetivo de lidar com operações que sejam
    relatadas com usuários. Como troca de senha, configuração de perfil,
    atualização cadastral e afins.
    
    O primeiro método adicionado a ele é o
    Create_New_User, que será o responsável por executar a lógica de criação
    de usuário. Porém, agora esse método apenas funciona via get.
    
    Nesse
    funcionamento GET,ele retorna o formulário de cadastro de usuário
    (form-create_user).
    
    Nesse controller, também foi adicionado mapping para um melhor entedimento e a fácil criação de urls, pois o controller fica com o caminho User, e o método Create_New_User fica com o caminho de url com o mesmo nome.

---
+++ main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.website.main_aplication_steins_now.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/User")
+public class UserController {
+    @GetMapping("/Create_New_User")
+    public String Create_New_User(){
+        return "form_create_user";
+    }
+}
