2ed97d6e3e72d84645e441c919cd6980ce27ad76
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2023-06-23 22:38:07 -0300

    feat(Controller): Criado construtor para injeção de serviço
    No controller, criei um construtor que recebe um objeto UserService como
    injeção de depedência.

--- main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/controller/UserController.java
+++ main_aplication_steins_now/src/main/java/com/website/main_aplication_steins_now/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.website.main_aplication_steins_now.controller;
 
 
+import com.website.main_aplication_steins_now.models.UserModel;
+import com.website.main_aplication_steins_now.repository.UserRepository;
+import com.website.main_aplication_steins_now.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/User")
 public class UserController {
+
+    public UserService userService;
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
     @GetMapping("/Create_New_User")
     public String Create_New_User(Model model){
         model.addAttribute("user",new UserModel());
